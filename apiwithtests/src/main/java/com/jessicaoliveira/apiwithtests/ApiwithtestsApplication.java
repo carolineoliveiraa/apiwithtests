348e74c664d7b1a434861c19c570030e466be5aa
Jéssica Caroline <jessicacaroline3213@hotmail.com>
2024-07-16 01:53:43 -0300

    created application

---
+++ apiwithtests/src/main/java/com/jessicaoliveira/apiwithtests/ApiwithtestsApplication.java
@@ -0,0 +1,13 @@
+package com.jessicaoliveira.apiwithtests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiwithtestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiwithtestsApplication.class, args);
+	}
+
+}
---
+++ apiwithtests/src/test/java/com/jessicaoliveira/apiwithtests/ApiwithtestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jessicaoliveira.apiwithtests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiwithtestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
