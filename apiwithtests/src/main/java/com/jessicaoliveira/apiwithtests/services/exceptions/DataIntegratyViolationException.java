d0ece929a5b1cb0279b70786753c06faf7c6b25d
Jéssica Caroline <jessicacaroline3213@hotmail.com>
2024-08-06 23:03:27 -0300

    updating

--- apiwithtests/src/main/java/com/jessicaoliveira/apiwithtests/services/exceptions/DataIntegratyViolationException.java
+++ apiwithtests/src/main/java/com/jessicaoliveira/apiwithtests/services/exceptions/DataIntegratyViolationException.java
@@ -1,10 +1,11 @@
 package com.jessicaoliveira.apiwithtests.services.exceptions;
 
 public class DataIntegratyViolationException extends RuntimeException{
+    
 
     public DataIntegratyViolationException(String message) {
         super(message);
 
-        
+
     }
 }
