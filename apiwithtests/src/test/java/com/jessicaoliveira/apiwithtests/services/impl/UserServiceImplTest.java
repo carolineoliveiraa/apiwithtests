fcadf808839920e13851a9e672af3d385eca3c24
Jéssica Caroline <jessicacaroline3213@hotmail.com>
2024-08-09 23:44:41 -0300

    test created of service

--- apiwithtests/src/test/java/com/jessicaoliveira/apiwithtests/services/impl/UserServiceImplTest.java
+++ apiwithtests/src/test/java/com/jessicaoliveira/apiwithtests/services/impl/UserServiceImplTest.java
@@ -3,21 +3,22 @@ package com.jessicaoliveira.apiwithtests.services.impl;
 import com.jessicaoliveira.apiwithtests.domain.User;
 import com.jessicaoliveira.apiwithtests.domain.dto.UserDTO;
 import com.jessicaoliveira.apiwithtests.repositories.UserRepository;
+import com.jessicaoliveira.apiwithtests.services.exceptions.DataIntegratyViolationException;
 import com.jessicaoliveira.apiwithtests.services.exceptions.ObjectNotFoundException;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.anyInt;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
@@ -27,6 +28,8 @@ class UserServiceImplTest {
     public static final String NAME = "jessica";
     public static final String EMAIL = "jessica@mail.com";
     public static final String PASSWORD = "123";
+    public static final String OBJETO_NAO_ENCONTRADO = "Objeto não encontrado";
+    public static final int INDEX = 0;
     @InjectMocks
     private UserServiceImpl service;
     @Mock
@@ -35,12 +38,17 @@ class UserServiceImplTest {
     private ModelMapper mapper;
     private User user;
     private UserDTO userDTO;
-    private Optional optionalUser;
+    private Optional<User> optionalUser;
 
     @BeforeEach
     void setUp() {
         MockitoAnnotations.openMocks(this);
+        startUser();
+    }
+
+    private void startUser(){
         user = new User(ID, NAME, EMAIL, PASSWORD);
+        userDTO = new UserDTO(ID, NAME, EMAIL, PASSWORD);
         optionalUser = Optional.of(user);
     }
 
@@ -60,22 +68,57 @@ class UserServiceImplTest {
     @Test
     void whenFindByIdThenReturnAnObjectNotFoundException(){
 
-        when(repository.findById(anyInt())).thenThrow(new ObjectNotFoundException("Objeto não encontrado"));
+        when(repository.findById(anyInt())).thenThrow(new ObjectNotFoundException(OBJETO_NAO_ENCONTRADO));
 
         try {
             service.findById(ID);
         }catch (Exception ex){
             assertEquals(ObjectNotFoundException.class, ex.getClass());
-            assertEquals("Objeto não encontrado", ex.getMessage());
+            assertEquals(OBJETO_NAO_ENCONTRADO, ex.getMessage());
         }
     }
 
     @Test
-    void findAll() {
+    void whenFindAllThenReturnAnListOfUsers() {
+        when(repository.findAll()).thenReturn(List.of(user));
+
+        List<User> response = service.findAll();
+
+        assertNotNull(response);
+        assertEquals(1, response.size());
+        assertEquals(User.class, response.get(INDEX).getClass());
+
+        assertEquals(ID, response.get(INDEX).getId());
+        assertEquals(NAME, response.get(INDEX).getName());
+        assertEquals(EMAIL, response.get(INDEX).getEmail());
+        assertEquals(PASSWORD, response.get(INDEX).getPassword());
     }
 
     @Test
-    void create() {
+    void whenCreateTheReturnSuccess() {
+        when(repository.save(any())).thenReturn(user);
+
+        User response = service.create(userDTO);
+
+        assertNotNull(response);
+        assertEquals(User.class, response.getClass());
+        assertEquals(ID, response.getId());
+        assertEquals(NAME, response.getName());
+        assertEquals(EMAIL, response.getEmail());
+        assertEquals(PASSWORD, response.getPassword());
+    }
+ @Test
+    void whenCreateTheReturnAnDataIntegrityViolationException() {
+        when(repository.findByEmail(anyString())).thenReturn(optionalUser);
+
+        try {
+            optionalUser.get().setId(1);
+            service.create(userDTO);
+        }catch (Exception ex){
+            assertEquals(DataIntegratyViolationException.class, ex.getClass());
+            assertEquals("E-mail já cadastrado no sistema", ex.getMessage());
+        }
+
     }
 
     @Test
@@ -84,11 +127,5 @@ class UserServiceImplTest {
 
     @Test
     void delete() {
-    }
-
-    private void startUser(){
-        user = new User(ID, NAME, EMAIL, PASSWORD);
-        userDTO = new UserDTO(ID, NAME, EMAIL, PASSWORD);
-        optionalUser = Optional.of(new User(ID, NAME, EMAIL, PASSWORD));
     }
 }
