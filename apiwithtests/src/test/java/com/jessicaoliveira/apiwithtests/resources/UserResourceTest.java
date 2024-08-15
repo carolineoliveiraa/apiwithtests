2e035cddd0900a3c4adf94dd5ff4b6e283e45d8f
Jéssica Caroline <jessicacaroline3213@hotmail.com>
2024-08-14 23:51:28 -0300

    updating

--- apiwithtests/src/test/java/com/jessicaoliveira/apiwithtests/resources/UserResourceTest.java
+++ apiwithtests/src/test/java/com/jessicaoliveira/apiwithtests/resources/UserResourceTest.java
@@ -7,15 +7,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.ResponseEntity;
 
-import java.util.Optional;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.when;
@@ -74,6 +72,7 @@ class UserResourceTest {
 
     @Test
     void findAll() {
+        
     }
 
     @Test
